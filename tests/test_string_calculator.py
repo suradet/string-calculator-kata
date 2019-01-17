44dda6d98dd83d84d838b4f08aba47ab02228414
Suradet Jitprapaikulsarn <suradet.j@gmail.com>
2019-01-17 13:34:25 -0500

    Refactor the tests by combining test cases.

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -1,5 +1,6 @@
 """Test cases for the string calculator."""
 
+import pytest
 from string_calculator.string_calculator import add
 
 
@@ -13,3 +14,17 @@ def test_input_with_empty_string():
 def test_input_with_one_number():
     """Input with one number."""
     assert add("123") == 123
+
+
+# -----------------------------------------------------------------------------
+@pytest.mark.parametrize(
+    "num_str, expected",
+    [
+        ("", 0),
+        ("456", 456),
+        ("-789", -789),
+    ]
+)
+def test_adding_numbers(num_str, expected):
+    """Add the numbers from the input string."""
+    assert add(num_str) == expected
