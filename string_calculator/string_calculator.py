6b87fcfad609f5c7c8e35d5392f7faceb650a5ef
Suradet Jitprapaikulsarn <suradet.j@gmail.com>
2019-01-17 16:42:53 -0500

    Refactor the code.

--- string_calculator/string_calculator.py
+++ string_calculator/string_calculator.py
@@ -1,26 +1,42 @@
 """A string calculator."""
 
 
-def add(numbers: str) -> int:
-    """Add the numbers."""
-    if numbers == "":
-        return 0
+# -----------------------------------------------------------------------------
+def parse_num_str(num_str: str) -> list:
+    """Parse the num_str from the input string."""
+    if num_str == "":
+        return [0]
 
     delimiter = ','
 
     def split(string_):
         return string_.split(delimiter)
 
-    lines = numbers.splitlines()
+    lines = num_str.splitlines()
     if "//" in lines[0].strip():
         delimiter = lines[0][2]
         lines = lines[1:]
     # Flatten the list of lists
     num_str_list = sum(map(split, lines), [])
     values = map(int, num_str_list)
+    return list(values)
+
+
+# -----------------------------------------------------------------------------
+def handle_negative_numbers(values: list):
+    """Raise exception for negative numbers."""
     negative_numbers = list(filter(lambda x: x < 0, values))
     if negative_numbers != []:
         message = "negatives not allowed: "
         message += ", ".join(map(str, negative_numbers))
         raise ValueError(message)
-    return sum(map(int, num_str_list))
+
+
+# -----------------------------------------------------------------------------
+def add(num_str: str) -> int:
+    """Add the num_str."""
+    values = parse_num_str(num_str)
+
+    handle_negative_numbers(values)
+
+    return sum(values)
