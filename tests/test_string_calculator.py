5274f124b8cb627b8cfab4d621e425a67c483012
Suradet Jitprapaikulsarn <suradet.j@gmail.com>
2019-01-17 13:35:09 -0500

    Removing duplicate test cases.

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -5,18 +5,6 @@ from string_calculator.string_calculator import add
 
 
 # -----------------------------------------------------------------------------
-def test_input_with_empty_string():
-    """Input with empty string."""
-    assert add("") == 0
-
-
-# -----------------------------------------------------------------------------
-def test_input_with_one_number():
-    """Input with one number."""
-    assert add("123") == 123
-
-
-# -----------------------------------------------------------------------------
 @pytest.mark.parametrize(
     "num_str, expected",
     [
